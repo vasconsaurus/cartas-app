175e18e8408fbced5e03fe7919aa5d40b5889733
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-09-15 11:56:21 -0300

    added card component to it's own folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState } from "react";
+import Card from "../src/components/cards/Card.js"
 
 function App() {
   const cardContent = {
@@ -21,23 +22,6 @@ function App() {
   }
 
   const shuffledCards = shuffle(Object.entries(cardContent))
-
-  function Card({cardName, cardText}) {
-
-    const [flip, setFlip] = useState(false);
-
-    return (
-      <div className={`card ${flip ? 'flip' : ''}`}>
-        <div className="front" onClick={() => setFlip(!flip)}>
-          <h3>click me</h3>
-        </div>
-        <div className="back" onClick={() => setFlip(!flip)}>
-          <h3>{cardName}</h3>
-          <p>{cardText}</p>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <div className="cards_container">
